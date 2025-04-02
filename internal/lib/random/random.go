2eb7a1f8a418862716d7797913f1d666e875e538
Sergey Palshin <filroy.gamer@gmail.com>
2025-04-03 03:13:32 +0700

    make random string generator for aliases

---
+++ internal/lib/random/random.go
@@ -0,0 +1,26 @@
+package random
+
+//не писал сам, взял из видео
+
+//TODO: make generator test
+
+import (
+	"math/rand"
+	"time"
+)
+
+// NewRandomString generates random string with given size
+func NewRandomString(size int) string{
+	rnd := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	chars := []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+	"abcdefghijklmnopqrstuvwxyz" +
+	"0123456789")
+
+	b := make([]rune, size)
+	for i := range b{
+		b[i] = chars[rnd.Intn(len(chars))]
+	}
+
+	return string(b)
+}
